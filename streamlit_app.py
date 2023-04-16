d140a7ca389a781d6639519a1b464b023014f9fe
lliahkim <130876987+lliahkim@users.noreply.github.com>
2023-04-15 21:17:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,6 @@ streamlit.text("Make your own smoothie")
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.multiselect("Pick some fruit for your smoothie", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
