803d6cf4641cf3b9da71701e7abe4bafd9681643
lliahkim <130876987+lliahkim@users.noreply.github.com>
2023-04-15 21:11:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,7 @@ import streamlit
 
 streamlit.title("My parent new healthy diner")
 streamlit.header("Breakfast Menu")
-streamlit.text("Omelette - $5")
+streamlit.text("🐔 Omelette - $5")
+streamlit.text("🥑🍞 Avocado Toast - $6")
+streamlit.text("🥣 Oatmeal and raisin - $5")
+streamlit.text("Make your own smoothie")
